52b36010fda8012d1da47832dbe284997fcaf1c6
yashjain-99 <yashj133.yj@gmail.com>
2025-06-12 09:58:09 +0530

    [API] Added core app for to store all models

--- api/task_manager/core/models.py
+++ api/task_manager/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    effort_days = models.PositiveIntegerField()  # Effort in days
+    due_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
